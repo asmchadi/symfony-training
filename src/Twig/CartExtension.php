80346886e7c9be0562b98ef77ffc9711f1e7c1ff
Issam KHADIRI <khadiri.issam@gmail.com>
2021-03-13 17:48:32 +0100

    Add twig filter and functions

---
+++ src/Twig/CartExtension.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Entity\Order;
+use App\Entity\OrderLine;
+use App\Entity\Product;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class CartExtension extends AbstractExtension
+{
+    const LENGHT = 20;
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('totalCart', [$this, 'computeTotal']),
+            new TwigFunction('totalOrderLine', [$this, 'computeLineTotal']),
+        ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('minimize', [$this, 'minimizeString']),
+        ];
+    }
+
+    /**
+     * Computes and returns the total amount of the given cart.
+     *
+     * @param Order $order the order instance
+     *
+     * @return string the formatted total
+     */
+    public function computeTotal(Order $order): string
+    {
+        $total = 0.0;
+        foreach ($order->getOrderLines() as $line) {
+            $total += ($line->getQuantity() * $line->getProduct()->getUnitPrice());
+        }
+
+        return \number_format($total, 2, ',', ' ');
+    }
+
+    /**
+     * Computes and returns the total amooutof the given order line.
+     *
+     * @param OrderLine $line the order line
+     *
+     * @return string the formatted total
+     */
+    public function computeLineTotal(OrderLine $line): string
+    {
+        return \number_format(
+            $line->getQuantity()*$line->getProduct()->getUnitPrice(),
+            2,
+            ',',
+            ' '
+        );
+    }
+
+    /**
+     * Filter to cut the product's label to keep only first n characters.
+     *
+     * @param Product $product the product instance
+     *
+     * @return string the resulting string
+     */
+    public function minimizeString(Product $product): string
+    {
+        return \strlen($product->getLabel()) > 0 ? \sprintf(
+            '%s...',
+            \mb_substr($product->getLabel(), 0, self::LENGHT)
+        ) : $product->getLabel();
+    }
+}
